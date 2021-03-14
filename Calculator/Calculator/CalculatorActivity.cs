ec5ffcd4d0ca36665c2c0d073d47039cac5ad15f
iSamsungS <32791265+iSamsungS@users.noreply.github.com>
2021-03-14 22:51:22 +0200

    tava ning tipi kalkulaator

---
+++ Calculator/Calculator/CalculatorActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    [Activity(Label = "CalculatorActivity")]
+    public class CalculatorActivity : Activity
+    {
+        EditText _firstEditText;
+        EditText _secondEditText;
+        TextView _answerText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.calculator_layout);
+
+            _firstEditText = FindViewById<EditText>(Resource.Id.firstNumberEditText);
+            _secondEditText = FindViewById<EditText>(Resource.Id.secondNumberEditText);
+            var addButton = FindViewById<Button>(Resource.Id.addButton);
+            var subtractButton = FindViewById<Button>(Resource.Id.subtractButton);
+            var multiplyButton = FindViewById<Button>(Resource.Id.multiplyButton);
+            var divideButton = FindViewById<Button>(Resource.Id.divideButton);
+            _answerText = FindViewById<TextView>(Resource.Id.answerTextView);
+
+            addButton.Click += AddButton_Click;
+            subtractButton.Click += subtractButton_Click;
+            multiplyButton.Click += multiplyButton_Click;
+            divideButton.Click += divideButton_Click;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {                                    
+            var answer = int.Parse(_firstEditText.Text) + int.Parse(_secondEditText.Text);
+            _answerText.Text = answer.ToString();
+        }
+
+        void subtractButton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(_firstEditText.Text) - int.Parse(_secondEditText.Text);
+            _answerText.Text = answer.ToString();
+        }
+        void multiplyButton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(_firstEditText.Text) * int.Parse(_secondEditText.Text);
+            _answerText.Text = answer.ToString();
+        }
+        void divideButton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(_firstEditText.Text) / int.Parse(_secondEditText.Text);
+            _answerText.Text = answer.ToString();
+        }
+    }
+}
---
+++ Calculator/Calculator/TipcalcActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    [Activity(Label = "TipcalcActivity")]
+    public class TipcalcActivity : Activity
+    {
+        EditText _subTotalEditText;
+        TextView _tipTextView;
+        SeekBar _tipSeekbar;
+        TextView _billTotalTextView;
+        TextView _tipTotalTextView;
+        Button _kalkuleeri;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.tipcalc_layout);
+            // Create your application here
+
+            _subTotalEditText = FindViewById<EditText>(Resource.Id.subTotalEditText);
+            _tipTextView = FindViewById<TextView>(Resource.Id.tipTextView);
+            _tipSeekbar = FindViewById<SeekBar>(Resource.Id.tipSeekBar);
+            var _kalkuleeri = FindViewById<Button>(Resource.Id.kalkuleeri);
+            _billTotalTextView = FindViewById<TextView>(Resource.Id.billTotalTextView);
+            _tipTotalTextView = FindViewById<TextView>(Resource.Id.tipTotalTextView);
+
+            _tipSeekbar.ProgressChanged += TipSeekbar_ProgressChanged;
+
+            _kalkuleeri.Click += CalculateButton_Click;
+        }
+
+
+        private void TipSeekbar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            _tipTextView.Text = e.Progress.ToString();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            float precentage = float.Parse(_tipTextView.Text) / 100;
+            var tipTotal = float.Parse(_subTotalEditText.Text) * precentage;
+            var billTotal = float.Parse(_subTotalEditText.Text) + tipTotal;
+            _tipTotalTextView.Text = tipTotal.ToString();
+            _billTotalTextView.Text = billTotal.ToString();
+        }
+    }
+}
